bebe4fb06e8a4b673892aaeccd7b4f283043c48d
Bartek Olszanowski <baloo@int.pl>
2022-03-09 12:13:19 +0100

    barcode init

--- src/app/components/coupon-code/coupon-code.component.ts
+++ src/app/components/coupon-code/coupon-code.component.ts
@@ -1,15 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+const jsBarcode = require('jsbarcode');
 
 @Component({
   selector: 'app-coupon-code',
   templateUrl: './coupon-code.component.html',
   styleUrls: ['./coupon-code.component.scss'],
 })
-export class CouponCodeComponent implements OnInit {
+export class CouponCodeComponent implements OnInit, AfterViewInit {
   @Input() code: string;
+  @ViewChild('barcode') barcodeEl: ElementRef<HTMLImageElement>;
 
   constructor() { }
+  ngAfterViewInit(): void {
+    console.log(this.barcodeEl);
+    jsBarcode(this.barcodeEl.nativeElement, 'M957340');
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // jsBarcode(this.barcodeEl, 'Hi!');
+  }
 
 }
